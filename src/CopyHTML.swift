fdeaf9f2bf96493afd57cf0772debd4f1a35658d
Charlie Chao <charliechao@outlook.com>
2017-10-19 21:55:15 -0700

    Compile scripts with Swift 4. Add CopyMD.

--- src/CopyHTML.swift
+++ src/CopyHTML.swift
@@ -3,11 +3,11 @@ import Cocoa
 let argv = CommandLine.arguments[1].characters.split(separator:" ", maxSplits: 1).map(String.init)
 let url = argv.count > 0 ? argv[0] : "about:blank"
 let title = argv.count > 1 ? argv[1] : "Untitled"
-let pasteboard = NSPasteboard.general()
-let type = NSPasteboardTypeHTML
+let pasteboard = NSPasteboard.general
+let type = NSPasteboard.PasteboardType.html
 
 pasteboard.declareTypes([type], owner: nil)
 pasteboard.setString("<a href='\(url)'>\(title)</a>", forType: type)
-pasteboard.setString(title + " — " + url, forType: NSPasteboardTypeString)
+pasteboard.setString(title + " — " + url, forType: NSPasteboard.PasteboardType.string)
 
 print(title)
