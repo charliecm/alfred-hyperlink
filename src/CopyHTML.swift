5ec13dff13a0fa0fe61ea532dbc08d01fa58f4c0
Charlie Chao <charliechao@outlook.com>
2020-01-11 09:59:26 -0800

    Fix copy script for Catalina.

--- src/CopyHTML.swift
+++ src/CopyHTML.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-let argv = CommandLine.arguments[1].characters.split(separator:" ", maxSplits: 1).map(String.init)
+let argv = CommandLine.arguments[1].split(separator:" ", maxSplits: 1).map(String.init)
 let url = argv.count > 0 ? argv[0] : "about:blank"
 let title = argv.count > 1 ? argv[1] : "Untitled"
 let pasteboard = NSPasteboard.general
